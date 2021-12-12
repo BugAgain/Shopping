2be779584e4470a23ec95e622af85395bde8c279
BugAgain <bug.errorcode5@gmail.com>
2021-12-12 15:40:18 +0400

    product create fix image path

--- Shopping/Controllers/ProductsController.cs
+++ Shopping/Controllers/ProductsController.cs
@@ -53,7 +53,7 @@ namespace Shopping.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Color,Price")] Product product)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ImagePath,Color,Price")] Product product)
         {
             if (ModelState.IsValid)
             {
